7fdfd144efdcae73e8b521c430bf5fe9a5a3e703
Bryan Veloso <bryan@avalonstar.com>
2018-12-15 16:07:54 -0800

    Hook Participant up to Firestore.

--- src/components/Participant.js
+++ src/components/Participant.js
@@ -1,16 +1,19 @@
 import React from 'react';
-import PropTypes from 'prop-types';
+
+import * as Providers from 'providers';
 
 import { ChevronRight } from 'react-feather';
 import { Capsule } from 'styles';
 
-const Participant = props => (
+const Participant = () => (
   <Capsule.Wrapper>
     <Capsule.Title>
       <ChevronRight color="#eaf56b" size={18} />
       {'Live Now'}
     </Capsule.Title>
-    <Capsule.Stat>{props.name}</Capsule.Stat>
+    <Providers.Participant>
+      {({ caster }) => <Capsule.Stat>{caster}</Capsule.Stat>}
+    </Providers.Participant>
   </Capsule.Wrapper>
 );
 
